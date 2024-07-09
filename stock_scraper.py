83d66c9301d13c9063142d5592ded17583ba35a3
suyogrnerkar <suyogrnerkar@gmail.com>
2024-07-09 13:43:43 -0400

    Added new welcome message.

--- stock_scraper.py
+++ stock_scraper.py
@@ -30,6 +30,7 @@ def get_stock_prices(ticker_list):
 
 def print_stock_scraper_welcome_message():
     print('Welcome to my stock scraper.')
+    print('--- We show stocks information here ---')
 
 def main():
     all_tickers = get_stock_tickers()
